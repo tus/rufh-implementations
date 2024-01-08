5f1a6aee797a20557e745694048f702d69c5e56d
Marius <marius.kleidl@gmail.com>
2024-01-08 16:49:10 +0100

    Update main.go

--- clients/go/main.go
+++ clients/go/main.go
@@ -108,17 +108,17 @@ func uploadCreationProcedure(file *os.File) (string, error) {
 			}
 
 			uploadOffset := header.Get("Upload-Offset")
-
 			if uploadOffset != "" {
-				log.Printf("Received 104 response. Server reported to have saved %s bytes\n", uploadOffset)
-				return nil
+				log.Printf("Received 104 response. Server reported to have saved %s bytes\n", uploadOffset
 			}
 
 			uploadUrl := header.Get("Location")
-			log.Printf("Received 104 response. Location is: %s\n", uploadUrl)
-
-			if err := saveUploadState(uploadUrl); err != nil {
-				log.Printf("failed to write upload state file: %s", err)
+			if uploadUrl != "" {
+				log.Printf("Received 104 response. Location is: %s\n", uploadUrl)
+	
+				if err := saveUploadState(uploadUrl); err != nil {
+					log.Printf("failed to write upload state file: %s", err)
+				}
 			}
 
 			return nil
